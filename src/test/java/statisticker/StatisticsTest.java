51d19c8f9f64761d06e1487d1f32912d12b7077a
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-10-09 12:41:31 +0530

    Update StatisticsTest.java

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -11,29 +11,32 @@ import org.junit.Test;
 
 public class StatisticsTest 
 {
+      @Test
+    public void reportsNaNForEmptyInput()
+    {
+        List<Float> emptyList = new ArrayList<>();
+
+        Statistics.Stats s = Statistics.getStatistics(emptyList);
+
+        Assert.assertTrue(s.average.isNaN());
+        Assert.assertTrue(s.min.isNaN());
+        Assert.assertTrue(s.max.isNaN());
+    }
+    
+    
     @Test
     public void reportsAverageMinMaxx()
     {
         Float[] numbers = {1.5f, 8.9f, 3.2f, 4.5f};
-        List<___> numberList = Arrays.asList(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
 
         Statistics.Stats s = Statistics.getStatistics(numberList);
 
         float epsilon = 0.001f;
-        assertEquals(s.average, 4.525f, epsilon);
-        assertEquals(s.min, 1.5f, epsilon);
+             assertEquals(s.min, 1.5f, epsilon);
         assertEquals(s.max, 8.9f, epsilon);
+        assertEquals(s.average, 4.525f, epsilon);
+   
     }
-    @Test
-    public void reportsNaNForEmptyInput()
-    {
-        List<___> emptyList = new ArrayList<___>();
-
-        Statistics.Stats s = Statistics.getStatistics(emptyList);
-
-        //All fields of computedStats (average, max, min) must be
-        //Float.NaN (not-a-number), as described in
-        //https://www.geeksforgeeks.org/nan-not-number-java/
-        //Design the asserts here and implement accordingly.
-    }
+   
 }
